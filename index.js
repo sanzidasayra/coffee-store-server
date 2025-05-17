f7c3f334c611aae5f1be0b51e37d1d97a4b367a0
sanzidasayra <sanzida7651@gmail.com>
2025-05-17 23:54:27 +0600

    card

--- index.js
+++ index.js
@@ -1,11 +1,75 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
-
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vdaznfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const coffeesCollection = client.db('coffeeDB').collection('coffees')
+
+
+    app.get('/coffees', async(req, res)=> {
+        // const cursor = coffeesCollection.find();
+        // const result = await cursor.toArray();
+        const result = await coffeesCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/coffees/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await coffeesCollection.findOne(query)
+        res.send(result)
+    })
+
+    
+
+    app.post('/coffees', async(req, res) => {
+        const newCoffee = req.body;
+        console.log(newCoffee);
+        const result = await coffeesCollection.insertOne(newCoffee);
+        res.send(result);
+    })
+
+
+    app.delete('/coffees/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await coffeesCollection.deleteOne(query)
+        res.send(result)
+    })
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Coffee Server is getting hotter.')
